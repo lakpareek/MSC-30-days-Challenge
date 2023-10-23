65ddea1313ffcd229374f3f45092d3df1fdff2d4
lakpareek <126066065+lakpareek@users.noreply.github.com>
2023-10-24 02:08:07 +0530

    Create merge-k-sorted-lists.cpp

---
+++ merge-k-sorted-lists.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		if(l1 == NULL){
+			return l2;
+		}
+		if(l2 == NULL){
+			return l1;
+		}
+		ListNode* newHead = NULL;
+		ListNode* newTail = NULL;
+		if(l1->val < l2->val){
+			newHead = l1;
+			newTail = l1;
+			l1 = l1->next;
+		}
+		else{
+			newHead = l2;
+			newTail = l2;
+			l2 = l2->next;
+		}
+		while(l1 != NULL and l2 != NULL){
+			if(l1->val < l2->val){
+				newTail->next = l1;
+				newTail = newTail->next;
+				l1 = l1->next;
+			}
+			else{
+				newTail->next = l2;
+				newTail = newTail->next;
+				l2 = l2->next;
+			}
+		}
+		if(l1 != NULL){
+			newTail->next = l1;
+			l1 = l1->next;
+		}
+		if(l2 != NULL){
+			newTail->next = l2;
+		}
+		return newHead;
+	}
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) {
+            return nullptr;
+        }
+        if (lists.size() == 1) {
+            return lists[0];
+        }
+
+        for(int i = 0; i<lists.size() - 1; i++){
+            ListNode* mergedList = mergeTwoLists(lists[i], lists[i+1]);
+            lists[i+1] = mergedList; 
+        }
+        return lists.back();
+    }
+
+};
